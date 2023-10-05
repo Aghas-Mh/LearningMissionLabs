3bca666fba96b21912b62e913eff02910c731315
PC <PC@DESKTOP-BSHVE6A>
2023-10-06 01:58:44 +0400

    Add project files.

---
+++ LearningMissionLabs/BLL/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace LearningMissionLabs.BLL
+{
+    public interface IStudentService
+    {
+        Task<bool> AddStudent(string email, string name);
+    }
+}
